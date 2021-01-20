4caa8949a7efbbfce391ae855c8e97c318cc15c9
Aswath S <aswath7862001@gmail.com>
2021-01-21 01:05:34 +0530

    added show method and mapped it to the route so as to render details of a single todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,11 @@
 class TodosController < ApplicationController
   def index
-    render plain: Todo.all.to_a
+    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    todo_string = Todo.find(id).to_pleasant_string
+    render plain: todo_string
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,5 @@
 class Todo < ActiveRecord::Base
-  
+  def to_pleasant_string
+    "#{id}. #{todo_text} is due at #{due_date.to_s(:long)} #{completed ? "[x]" : "[ ]"}"
+  end
 end
